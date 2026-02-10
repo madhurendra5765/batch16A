1bb7df0310af400be31e0dbc1be37065d14f45e9
madhurendra5765 <madhurendrakumar85@gmail.com>
2026-02-10 10:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ print ('My name is Madhurendra')
 a = 6
 b = 5
 c = a+b
-print('the sum of 2 numbers are : ' c)
+print ("the sum of 2 numbers are : ", c)
 
-print ("Name and sum is : " madhu,c)
+print ("The age of madh is" )
